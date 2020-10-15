c98a92aeddb084c8d7a3333a7b0de73e0b9510af
rojanandana <40118828+rojanandana@users.noreply.github.com>
2020-10-15 19:20:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,43 +14,42 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-// Set static folder
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const botName = 'FocusEdumatics_ChatBot';
 
-// Run when client connects
+
 io.on('connection', socket => {
     socket.on('joinRoom', ({ username, room }) => {
         const user = userJoin(socket.id, username, room);
 
         socket.join(user.room);
 
-        // Welcome current user
+       
         socket.emit('message', formatMessage(botName, 'Welcome to FocusEdumatics_Chat!'));
 
-        // Broadcast when a user connects
+        
         socket.broadcast
             .to(user.room).emit(
                 'message',
                 formatMessage(botName, `${user.username} has joined the chat`)
             );
 
-        // Send users and room info
+        
         io.to(user.room).emit('roomUsers', {
             room: user.room,
             users: getRoomUsers(user.room)
         });
     });
 
-    // Listen for chatMessage
+
     socket.on('chatMessage', msg => {
         const user = getCurrentUser(socket.id);
 
         io.to(user.room).emit('message', formatMessage(user.username, msg));
     });
 
-    // Runs when client disconnects
     socket.on('disconnect', () => {
         const user = userLeave(socket.id);
 
@@ -64,7 +63,7 @@ io.on('connection', socket => {
             );
 
 
-            // Send users and room info
+           
             io.to(user.room).emit('roomUsers', {
                 room: user.room,
                 users: getRoomUsers(user.room)
